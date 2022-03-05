07e7ed21a4896f52c329b2ef209b9f642c9a6efc
Anubhav Raina <anubhav7991@gmail.com>
2022-03-05 13:06:29 +0530

    change diffuse fn return

--- src/scene/material/matte.rs
+++ src/scene/material/matte.rs
@@ -4,7 +4,6 @@ use crate::geometry::vec3::Vector;
 use crate::geometry::vec3::Vector as Point;
 use crate::scene::ray::Ray;
 use crate::scene::intersect::Intersection;
-use rand::Rng;
 
 pub struct Matte {
     albedo: RgbColor,
@@ -21,20 +20,21 @@ impl Matte {
     /// N is the normal of the surface
     /// I is the incident vector
     /// As the angle increases, the light gets weaker
-    fn lambertian_diffuse(&self, incident: &Vector, normal: &Vector, hit: &Point) -> Ray {
+    fn lambertian_diffuse(&self, incident: &Vector, normal: &Vector, hit: &Point) -> Option<Ray> {
         // let dir = self.random_in_unit_sphere() + *normal;
         let dir = self.random_in_hemisphere(*incident) + *normal;
         if dir.len() < 0.001f64 {
-            return Ray::new(*hit, *normal);
+            // return Ray::new(*hit, *normal);
+            return None;
         }
-        Ray::new(*hit, dir)
+        Some(Ray::new(*hit, dir))
     }
 }
 
 impl Material for Matte {
     type Albedo = RgbColor;
     fn scatter(&self, hit_ray: &Ray, hit: &Intersection) -> Option<Ray> {
-        Some(self.lambertian_diffuse(&hit_ray.direction, &hit.normal, &hit.point))
+        self.lambertian_diffuse(&hit_ray.direction, &hit.normal, &hit.point)
     }
 
     fn albedo(&self) -> &Self::Albedo {
